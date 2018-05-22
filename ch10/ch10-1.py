2d7d725d3d9267a416fccf68fc9019e0d95cf2bb
q10242 <q10242@gmail.com>
2018-05-22 11:18:11 +0800

    Ch10 is done

---
+++ ch10/ch10-1.py
@@ -0,0 +1,18 @@
+langs = {'a','b','c','a','b','c'}
+print langs
+b = (1,2,3)
+print type(b)
+c = {'a':'b'}
+print type(c)
+langs2 = {'d'}
+print type(langs2)
+emptyset= set()
+emptydict={}
+print type(emptyset)
+print type(emptydict)
+
+
+fruit = ['apple','banana','dick','dick']
+y = set(fruit)
+print y
+print list(y)
